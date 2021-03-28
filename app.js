95a49361888cdc7f4fa959ce2e99c0d3c7b29d23
AlexeyEmshanov <alexey.emshanov@gmail.com>
2021-03-28 15:02:18 +0400

    fix: remove tag selectors from remove buttons according to rule 3.4

--- app.js
+++ app.js
@@ -45,6 +45,7 @@ var createNewTaskElement=function(taskString){
 
     deleteButton.className="btn delete-btn";
     deleteButtonImg.src='./remove.svg';
+    deleteButtonImg.className = "remove-img";
     deleteButton.appendChild(deleteButtonImg);
 
 
