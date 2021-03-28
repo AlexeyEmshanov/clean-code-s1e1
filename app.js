55072eb69df587222388ea32597bea6bde644cc2
AlexeyEmshanov <alexey.emshanov@gmail.com>
2021-03-28 17:05:49 +0400

    fix: remove tag selectors at label usage accordint to rule 3.4

--- app.js
+++ app.js
@@ -89,7 +89,7 @@ var editTask=function(){
     var listItem=this.parentNode;
 
     var editInput=listItem.querySelector('input[type=text]');
-    var label=listItem.querySelector("label");
+    var label=listItem.querySelector(".task");
     var editBtn=listItem.querySelector(".edit-btn");
     var containsClass=listItem.classList.contains("edit-mode");
     //If class of the parent is .editmode
